9b208ac3a4dde937e24c651c9bdfa0dc592abe07
clssxj <2495488958@qq.com>
2024-02-11 15:53:47 +0800

    创建项目后端

---
+++ Backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -0,0 +1,12 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/pk/")
+public class IndexController {
+    @RequestMapping("Index/")
+    public String Index(){
+        return "pk/Index.html";
+    }
+}
