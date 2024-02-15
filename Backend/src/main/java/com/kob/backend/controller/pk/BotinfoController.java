9e29ea1151678a24951312fbdb7152680f31a693
clssxj <2495488958@qq.com>
2024-02-15 22:12:56 +0800

    2.15版本

--- Backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ Backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -3,11 +3,17 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotinfoController {
     @RequestMapping("getBotinfo/")
-    public String getBotinfo(){
-        return "6";
+    public Map<String,String> getBotinfo(){
+        Map<String,String> bot1=new HashMap<>();
+        bot1.put("name","apple");
+        bot1.put("rating","1500");
+        return bot1;
     }
 }
