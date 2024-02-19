2b7b088b56d4c6cc6f14f8809b44e72cb62308f4
clssxj <2495488958@qq.com>
2024-02-19 12:49:47 +0800

    2.19版本

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PKIndexView from '../views/PK/PKIndexView'
+import RecordsIndexView from '../views/Records/RecordsIndexView'
+import RankListIndex from '../views/RankList/RankListIndexView'
+import UserBotIndex from '../views/Users/Bots/UserBotIndexView'
+import NotFound from '../views/Errors/NotFound'
 
 const routes = [
+  {
+    path:'/',
+    name:"home",
+    redirect:"/pk/"
+  },
+  {
+    path:"/pk/",
+    name:"pk_index",
+    component: PKIndexView,
+  },
+  {
+    path:"/records/",
+    name:"records_index",
+    component: RecordsIndexView,
+  },
+  {
+    path:"/ranklist/",
+    name:"ranklist_index",
+    component: RankListIndex,
+  },
+  {
+    path:"/users/mybots/",
+    name:"users_bot_index",
+    component: UserBotIndex,
+  },
+  {
+    path:"/404/",
+    name:"404",
+    component: NotFound,
+  },
+  {
+    path:"/:catchAll(.*)",
+    redirect:"/404/"
+  },
 ]
 
 const router = createRouter({
